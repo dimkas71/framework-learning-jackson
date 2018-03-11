26eed176211f10a2cf840d0cd7a5ad9389c84af9
dimkas71g@gmail.com <dimkas71g@gmail.com>
2018-03-11 16:02:25 +0200

    initial commit

---
+++ src/test/java/ua/compservice/test/IgnorePropertiesTest.java
@@ -0,0 +1,14 @@
+package ua.compservice.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IgnorePropertiesTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
